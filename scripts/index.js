8d02acd44cbf948a822a976b3b7c1e54c0c6b4bb
jvalori-fg <jvalori@firstgencom.com>
2021-03-19 14:41:49 -0400

    hook up buy now button

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,46 @@ import { restockAlerts } from './components/modal.js';
 
 $(document).ready(function(){
     restockAlerts();
+
+    $('[data-buy-now-button]').on('click', function(e){
+        buyNowButton(e)
+    })
 })
+let $this,
+    prodID,
+    productData;
+
+function buyNowButton(e){
+    $this = e.target;
+    prodID = $($this).attr('data-variant-id');
+
+    productData = {
+        items: [
+            {
+            quantity: 1,
+            id: prodID
+            }
+        ]
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/cart/add.js',
+        data: productData,
+        dataType: "json",
+        beforeSend: function(){
+            $($this).attr("disabled", true)
+            $($this).html("Loading...")
+        },
+        success: function(){
+            console.log("Success!")
+            $($this).attr("disabled", false)
+            $($this).html("Buy Now")
+        },
+        error: function(){
+            alert("Error!")
+        }
+    })
+    
+
+}
